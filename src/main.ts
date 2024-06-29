d1b41658f01099103d23de85bd11f353cd3b5fd5
Agatha <ryan.nantes4@gmail.com>
2024-06-29 15:09:28 -0400

    aaa

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');  // Configuração do CORS
+  app.enableCors({
+    origin: '*', // Permitir todas as origens; ajuste conforme necessário
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
