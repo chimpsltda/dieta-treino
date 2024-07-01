2a1743477625300b50cb7eb861f30335a3d0a183
FabigodUFG <142517751+FabigodUFG@users.noreply.github.com>
2024-07-01 15:04:04 -0400

    teste

--- src/dados_diario/dados.diario.service.ts
+++ src/dados_diario/dados.diario.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma, DadosDiario } from '@prisma/client';
-import { DatabaseService } from 'src/database/database.service';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient, DadosDiario } from '@prisma/client';
 import { CreateDadoDiarioDTO } from './dto/create.dados.diario.dto';
 import { UpdateDadoDiarioDto } from './dto/update.dados.diario.dto';
 
 @Injectable()
-export class DadosDiarioService {
-  constructor(private readonly databaseservice: DatabaseService) {}
+export class DadosDiarioService implements OnModuleDestroy {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async onModuleDestroy() {
+    await this.prisma.$disconnect();
+  }
 
   async insert(createDadoDiarioDto: CreateDadoDiarioDTO): Promise<DadosDiario> {
     const data = new Date(createDadoDiarioDto.data_atual || new Date());
@@ -16,19 +23,19 @@ export class DadosDiarioService {
     const existingData = await this.findOneByEmailAndDate(createDadoDiarioDto.email, createDadoDiarioDto.data_atual);
 
     if (existingData) {
-      return this.databaseservice.dadosDiario.update({
+      return this.prisma.dadosDiario.update({
         where: { codigo: existingData.codigo },
         data: createDadoDiarioDto,
       });
     } else {
-      return this.databaseservice.dadosDiario.create({
+      return this.prisma.dadosDiario.create({
         data: createDadoDiarioDto,
       });
     }
   }
 
   async findOneByEmailAndDate(email: string, data_atual: Date): Promise<DadosDiario | null> {
-    return this.databaseservice.dadosDiario.findFirst({
+    return this.prisma.dadosDiario.findFirst({
       where: {
         email: email,
         data_atual: data_atual,
@@ -46,13 +53,22 @@ export class DadosDiarioService {
       throw new Error('Dados diarios não encontrados para este email.');
     }
 
-    return this.databaseservice.dadosDiario.update({
+    return this.prisma.dadosDiario.update({
       where: { codigo: dadosDiario.codigo },
       data: updateDadoDiarioDto,
     });
   }
 
+  async update(codigo: number, updateDadoDiarioDto: UpdateDadoDiarioDto): Promise<DadosDiario> {
+    return this.prisma.dadosDiario.update({
+      where: {
+        codigo: codigo,
+      },
+      data: updateDadoDiarioDto,
+    });
+  }
+
   async showall(): Promise<DadosDiario[]> {
-    return this.databaseservice.dadosDiario.findMany();
+    return this.prisma.dadosDiario.findMany();
   }
 }
